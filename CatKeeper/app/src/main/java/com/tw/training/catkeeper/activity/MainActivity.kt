b6a6224fb4e6e5215b4453e080fca2f0bf3b7d1a
leojiang <jiangyanghaics@163.com>
2017-10-04 20:05:47 +0800

    added viewpager

--- CatKeeper/app/src/main/java/com/tw/training/catkeeper/activity/MainActivity.kt
+++ CatKeeper/app/src/main/java/com/tw/training/catkeeper/activity/MainActivity.kt
@@ -1,23 +1,42 @@
 package com.tw.training.catkeeper.activity
 
 import android.os.Bundle
+import android.os.Handler
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
 import com.tw.training.catkeeper.R
+import com.tw.training.catkeeper.adapter.BannerAdapter
 import com.tw.training.catkeeper.presenter.MainContract
 import com.tw.training.catkeeper.presenter.MainPresenter
+import com.tw.training.catkeeper.Utils.ViewPagerScroller
 
-class MainActivity : AppCompatActivity(), MainContract.View {
+class MainActivity : AppCompatActivity(), MainContract.View, ViewPager.OnPageChangeListener {
+    private val MSG_WHAT = 0
 
-    @BindView(R.id.left_tab) lateinit var leftTab: TextView
-    //    lateinit var leftTab: TextView
-    @BindView(R.id.right_tab) lateinit var rightTab: TextView
-//    lateinit var rightTab: TextView
+    @BindView(R.id.left_tab)
+    lateinit var mLeftTab: TextView
 
-    var presenter: MainContract.Presenter = MainPresenter(this)
+    @BindView(R.id.right_tab)
+    lateinit var mRightTab: TextView
+
+    @BindView(R.id.ad_viewpager)
+    lateinit var mViewPager: ViewPager
+
+    @BindView(R.id.indicator)
+    lateinit var mIndicator : ViewGroup
+
+    private var imageResIds = arrayOf(R.mipmap.cat_1, R.mipmap.cat_2, R.mipmap.cat_3, R.mipmap.cat_4)
+
+    private var presenter: MainContract.Presenter = MainPresenter(this)
+
+    private var previousPosition = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +44,18 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         setContentView(R.layout.activity_main)
         ButterKnife.bind(this@MainActivity)
 
+        setupViewPager()
         presenter.onStart()
+    }
 
-//        leftTab = findViewById(R.id.left_tab)
-//        rightTab = findViewById(R.id.right_tab)
+    override fun onResume() {
+        super.onResume()
+        handler.sendEmptyMessageDelayed(MSG_WHAT, 2000)
+    }
 
-//        leftTab.setOnClickListener { onLeftTabClicked() }
-//        rightTab.setOnClickListener { OnRightTabClicked() }
+    override fun onStop() {
+        super.onStop()
+        handler.removeMessages(MSG_WHAT);
     }
 
     override fun onDestroy() {
@@ -44,15 +68,58 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 
 
     @OnClick(R.id.right_tab)
-    fun OnRightTabClicked() = presenter.switchToMyCat()
+    fun onRightTabClicked() = presenter.switchToMyCat()
 
     override fun switchToNearbyCat() {
-        rightTab.isEnabled = true
-        leftTab.isEnabled = false
+        mRightTab.isEnabled = true
+        mLeftTab.isEnabled = false
     }
 
     override fun switchToMyCat() {
-        leftTab.isEnabled = true
-        rightTab.isEnabled = false
+        mLeftTab.isEnabled = true
+        mRightTab.isEnabled = false
     }
+
+    private fun setupViewPager() {
+        val mViewContainer = java.util.ArrayList<ImageView>()
+        for (i in 0 until imageResIds.size) {
+            val view = LayoutInflater.from(this).inflate(R.layout.viewpager_item, null) as ImageView
+            view.setImageResource(imageResIds[i])
+            mViewContainer.add(view)
+        }
+
+        mViewPager.adapter = BannerAdapter(mViewContainer)
+        mViewPager.addOnPageChangeListener(this)
+        mViewPager.currentItem = 1000
+
+        val pagerScroller = ViewPagerScroller(this)
+        pagerScroller.initViewPagerScroll(mViewPager)
+    }
+
+    override fun onPageScrollStateChanged(p0: Int) {
+    }
+
+    override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
+    }
+
+    override fun onPageSelected(position: Int) {
+        val newPosition = position % imageResIds.size
+        updateIndicator(previousPosition, newPosition)
+        previousPosition = newPosition
+    }
+
+    private fun updateIndicator(pre: Int, now: Int) {
+        var dot = mIndicator.getChildAt(pre) as ImageView
+        dot.setImageResource(R.drawable.dot_bg_white)
+        dot = mIndicator.getChildAt(now) as ImageView
+        dot.setImageResource(R.drawable.dot_bg_green)
+    }
+
+    private val handler = object : Handler() {
+        override fun handleMessage(msg: android.os.Message) {
+            mViewPager.currentItem = mViewPager.currentItem + 1
+            this.sendEmptyMessageDelayed(MSG_WHAT, 4000)
+        }
+    }
+
 }
