9e5bbd667fe289d4f62d4d09a6b2e55f8be8e1ba
leojiang <jiangyanghaics@163.com>
2017-10-04 20:05:47 +0800

    added viewpager

---
+++ CatKeeper/app/src/main/java/com/tw/training/catkeeper/adapter/MyPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.tw.training.catkeeper.adapter
+
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+
+class MyPagerAdapter(private val imageList: List<ImageView>) : PagerAdapter() {
+
+    override fun getCount(): Int {
+        return Integer.MAX_VALUE
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val iv = imageList[position % imageList.size]
+        container as ViewPager
+        container.addView(iv)
+        return iv
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container as ViewPager
+        container.removeView(imageList[position % imageList.size])
+    }
+}
